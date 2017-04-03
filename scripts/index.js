be215accf36ae925adda03bcb056820bf8c940a6
jh2k <734055405@qq.com>
2017-04-03 15:13:40 +0800

    update

--- scripts/index.js
+++ scripts/index.js
@@ -10,3 +10,19 @@ jWindow.scroll(function(){
         $('#navbar-cx .active a').addClass('transparent-cx2');
     }
 });
+
+
+    $(function () {
+        $('#summary').carousel();
+        $('#navbar-cx .navbar-collapse a').click(function (e) {
+            var href = $(this).attr('href');
+            var tabId = $(this).attr('data-tab');
+            if ('#' !== href) {
+                e.preventDefault();
+                  $(document).scrollTop($(href).offset().top - 70);
+                if (tabId) {
+                    $('#nav-tabs-cx a[href=#' + tabId + ']').tab('show');
+                }
+            }
+        });
+    });
